e618eb277337983f625358e693c089497f65136f
Jonathan Gamble <jdgamble555@gmail.com>
2024-01-04 18:48:35 -0600

    cache test

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,13 @@
 import type { PageServerLoad } from './$types';
 
 
-export const load = (async () => {
+export const load = (async ({ setHeaders }) => {
 
     const n = Math.random();
+
+    setHeaders({
+        'Cache-Control': 'max-age=31536000, immutable'
+    });
 
     return {
         n
